84454592cc6d4c90a8e52af1a26372e511257688
Fabio Muratore <fabio.muratore@famura.net>
2020-05-04 19:17:10 +0200

    Initial commit

--- Pyrado/pyrado/environments/mujoco/wam.py
+++ Pyrado/pyrado/environments/mujoco/wam.py
@@ -4,6 +4,7 @@ import os.path as osp
 from init_args_serializer import Serializable
 
 import pyrado
+from pyrado.spaces.base import Space
 from pyrado.spaces.singular import SingularStateSpace
 from pyrado.tasks.base import Task
 from pyrado.environments.mujoco.base import MujocoSimEnv
@@ -108,20 +109,27 @@ class WAMBallInCupSim(MujocoSimEnv, Serializable):
         model_path = osp.join(pyrado.MUJOCO_ASSETS_DIR, 'wam_cup.xml')
         super().__init__(model_path, frame_skip, max_steps, task_args)
 
+        self.p_gains = np.array([200, 300, 100, 100, 10, 10, 2.5])
+        self.d_gains = np.array([7, 15, 5, 2.5, 0.3, 0.3, 0.05])
+
         self.camera_config = dict(
             trackbodyid=0,  # id of the body to track
             elevation=-30,  # camera rotation around the axis in the plane
             azimuth=180  # camera rotation around the camera's vertical axis
         )
 
+    @property
+    def torque_space(self) -> Space:
+        return self._torque_space
+
     @classmethod
     def get_nominal_domain_param(cls) -> dict:
         return dict()
 
     def _create_spaces(self):
-        # Action space
-        max_act = np.array([150., 125., 40., 60., 5., 5., 2.])
-        self._act_space = BoxSpace(-max_act, max_act)
+        # Torque space
+        max_torque = np.array([150., 125., 40., 60., 5., 5., 2.])
+        self._torque_space = BoxSpace(-max_torque, max_torque)
 
         # State space
         state_shape = np.concatenate([self.sim.data.qpos, self.sim.data.qvel]).shape
@@ -134,10 +142,12 @@ class WAMBallInCupSim(MujocoSimEnv, Serializable):
         init_state = np.concatenate([init_pos, self.init_qvel.copy()]).ravel()
         self._init_space = SingularStateSpace(init_state)
 
+        # Action space
+        max_act = np.full((6,), pyrado.inf)
+        self._act_space = BoxSpace(-max_act, max_act)
+
         # Observation space
-        obs_shape = self.observe(max_state).shape
-        max_obs = np.full(obs_shape, pyrado.inf)
-        self._obs_space = BoxSpace(-max_obs, max_obs)
+        self._obs_space = BoxSpace(np.array([0]), np.array([self.max_steps]))
 
     def _create_task(self, task_args: [dict, None] = None) -> Task:
         # TODO: Formulate proper task/reward
@@ -148,7 +158,26 @@ class WAMBallInCupSim(MujocoSimEnv, Serializable):
         return DesStateTask(self.spec, state_des, ZeroPerStepRewFcn())
 
     def _mujoco_step(self, act: np.ndarray):
-        self.sim.data.qfrc_applied[:7] = act
+        # act = (des_qpos, des_qvel) with dim=(6,) is zero centered, therefore the init_pos is added
+        des_pos = self._init_space.sample_uniform()[:7]
+        des_pos[1] += act[0]
+        des_pos[3] += act[1]
+        des_pos[5] += act[2]
+        des_vel = np.zeros_like(des_pos)
+        des_vel[1] += act[3]
+        des_vel[3] += act[4]
+        des_vel[5] += act[5]
+
+        # Compute error on position and velocity
+        err_pos = des_pos - self.state[:7]
+        err_vel = des_vel - self.state[:7]
+
+        # Compute torques
+        torque = self.p_gains * err_pos + self.d_gains * err_vel
+        torque = self.torque_space.project_to(torque)
+
+        # Apply torque
+        self.sim.data.qfrc_applied[:7] = torque
         try:
             self.sim.step()
         except mujoco_py.builder.MujocoException as e:
@@ -160,7 +189,4 @@ class WAMBallInCupSim(MujocoSimEnv, Serializable):
         self.state = np.concatenate([pos, vel])
 
     def observe(self, state: np.ndarray) -> np.ndarray:
-        # Exclude rope and ball from observation. Only the position and velocity of the Barret WAM arm can be observed.
-        pos = state[:7]
-        vel = state[self.model.nq:self.model.nq + 7]
-        return np.concatenate([pos, vel])
+        return np.array([self._curr_step / self.max_steps])
