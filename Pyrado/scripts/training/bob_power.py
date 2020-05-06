3c6b46329c01ea76e47e4b91a69b40e9e1c014d1
Fabio Muratore <fabio.muratore@famura.net>
2020-05-06 08:55:02 +0200

    Updated bob_power.py

--- Pyrado/scripts/training/bob_power.py
+++ Pyrado/scripts/training/bob_power.py
@@ -2,33 +2,34 @@
 Train an agent to solve the Ball-on-Beam environment using Relative Entropy Search.
 """
 from pyrado.algorithms.power import PoWER
+from pyrado.environment_wrappers.action_normalization import ActNormWrapper
 from pyrado.environments.pysim.ball_on_beam import BallOnBeamSim
 from pyrado.logger.experiment import setup_experiment, save_list_of_dicts_to_yaml
-from pyrado.policies.features import FeatureStack, identity_feat, sin_feat, RBFFeat
+from pyrado.policies.features import FeatureStack, RBFFeat
 from pyrado.policies.linear import LinearPolicy
 
 if __name__ == '__main__':
     # Experiment (set seed before creating the modules)
-    ex_dir = setup_experiment(BallOnBeamSim.name, PoWER.name, 'lin-ident-sin', seed=101)
+    ex_dir = setup_experiment(BallOnBeamSim.name, PoWER.name, 'rbf', seed=101)
 
     # Environment
-    env_hparams = dict(dt=1/100., max_steps=500)
+    env_hparams = dict(dt=1/50., max_steps=300)
     env = BallOnBeamSim(**env_hparams)
+    env = ActNormWrapper(env)
 
     # Policy
     policy_hparam = dict(
-        # feats=FeatureStack([RBFFeat(num_feat_per_dim=50, bounds=env.obs_space.bounds, scale=None)])
-        feats=FeatureStack([identity_feat, sin_feat])
+        feats=FeatureStack([RBFFeat(num_feat_per_dim=7, bounds=env.obs_space.bounds, scale=None)])
     )
     policy = LinearPolicy(spec=env.spec, **policy_hparam)
 
     # Algorithm
     algo_hparam = dict(
         max_iter=100,
-        pop_size=200,
+        pop_size=100,
         num_rollouts=10,
-        num_is_samples=100,
-        expl_std_init=1.5,
+        num_is_samples=10,
+        expl_std_init=0.8,
         expl_std_min=0.02,
         symm_sampling=False,
         num_sampler_envs=4,
