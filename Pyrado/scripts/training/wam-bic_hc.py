3b1d05abc71ecda423b42365fcc3cd2550f81efd
Christian Eilers <christian_eilers@gmx.net>
2020-05-27 14:26:17 +0200

    Adding wam-bic policy for randomized env

--- Pyrado/scripts/training/wam-bic_hc.py
+++ Pyrado/scripts/training/wam-bic_hc.py
@@ -2,6 +2,9 @@
 Train an agent to solve the WAM Ball-in-cup environment using Hill Climbing.
 """
 from pyrado.algorithms.hc import HCNormal
+from pyrado.domain_randomization.domain_parameter import NormalDomainParam
+from pyrado.domain_randomization.domain_randomizer import DomainRandomizer
+from pyrado.environment_wrappers.domain_randomization import DomainRandWrapperLive
 from pyrado.environments.mujoco.wam import WAMBallInCupSim
 from pyrado.logger.experiment import setup_experiment, save_list_of_dicts_to_yaml
 from pyrado.policies.environment_specific import DualRBFLinearPolicy
@@ -9,30 +12,37 @@ from pyrado.policies.environment_specific import DualRBFLinearPolicy
 
 if __name__ == '__main__':
     # Experiment (set seed before creating the modules)
-    ex_dir = setup_experiment(WAMBallInCupSim.name, HCNormal.name, seed=101)
+    ex_dir = setup_experiment(WAMBallInCupSim.name, HCNormal.name, 'randomized', seed=101)
 
     # Environment
     env_hparams = dict(
-        max_steps=2500,
-        task_args=dict(factor=0.05)
+        max_steps=1000,
+        task_args=dict(factor=0.1)
     )
     env = WAMBallInCupSim(**env_hparams)
 
+    # Simple Randomizer
+    dp_nom = WAMBallInCupSim.get_nominal_domain_param()
+    randomizer = DomainRandomizer(
+        NormalDomainParam(name='cup_scale', mean=1.5, std=0.2, clip_lo=1., clip_up=2.)
+    )
+    env = DomainRandWrapperLive(env, randomizer)
+
     # Policy
     policy_hparam = dict(
-        rbf_hparam=dict(num_feat_per_dim=10, bounds=(0., 1.), scale=None),
+        rbf_hparam=dict(num_feat_per_dim=7, bounds=(0., 1.), scale=None),
         dim_mask=2
     )
     policy = DualRBFLinearPolicy(env.spec, **policy_hparam)
 
     # Algorithm
     algo_hparam = dict(
-        max_iter=100,
-        pop_size=5*policy.num_param,
+        max_iter=50,
+        pop_size=4*policy.num_param,
         expl_factor=1.05,
-        num_rollouts=1,
+        num_rollouts=3,
         expl_std_init=0.1,
-        num_sampler_envs=8,
+        num_sampler_envs=4,
     )
     algo = HCNormal(ex_dir, env, policy, **algo_hparam)
 
