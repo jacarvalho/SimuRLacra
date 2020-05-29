afc404065a9a268683307bd3828a5f123e5e0cbb
Fabio Muratore <fabio.muratore@famura.net>
2020-05-29 12:08:29 +0200

    Bug fix for mask creation via indices

--- Pyrado/pyrado/environment_wrappers/observation_partial.py
+++ Pyrado/pyrado/environment_wrappers/observation_partial.py
@@ -10,15 +10,15 @@ from init_args_serializer.serializable import Serializable
 class ObsPartialWrapper(EnvWrapperObs, Serializable):
     """ Environment wrapper which creates a partial observation by masking certain elements """
 
-    def __init__(self, wrapped_env: Env, mask: list = None, indices: list = None, keep_selected: bool = False):
+    def __init__(self, wrapped_env: Env, mask: list = None, idcs: list = None, keep_selected: bool = False):
         """
         Constructor
 
         :param wrapped_env: environment to wrap
         :param mask: mask out array, entries with 1 are dropped (behavior can be inverted by keep_selected=True)
-        :param indices: indices to drop, ignored if mask is specified.
-                        If the observation space is labeled, labels can be used as indices.
-        :param keep_selected: set to true to keep the mask entries with 1/the specified indices and drop the others.
+        :param idcs: indices to drop, ignored if mask is specified. If the observation space is labeled,
+                     the labels can be used as indices.
+        :param keep_selected: set to true to keep the mask entries with 1/the specified indices and drop the others
         """
         Serializable._init(self, locals())
 
@@ -26,13 +26,14 @@ class ObsPartialWrapper(EnvWrapperObs, Serializable):
 
         # Parse selection
         if mask is not None:
+            # Use explicit mask
             mask = np.array(mask, dtype=np.bool)
             if not mask.shape == wrapped_env.obs_space.shape:
                 raise pyrado.ShapeErr(given=mask, expected_match=wrapped_env.obs_space)
         else:
             # Parse indices
-            assert indices is not None, 'Either mask or indices must be specified'
-            mask = wrapped_env.obs_space.create_mask(wrapped_env.obs_space, indices)
+            assert idcs is not None, 'Either mask or indices must be specified'
+            mask = wrapped_env.obs_space.create_mask(idcs)
         # Invert if needed
         if keep_selected:
             self.keep_mask = mask
