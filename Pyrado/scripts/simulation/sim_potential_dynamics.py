40dd3b1ba8d41b04149889016ee2e1fedc743f6b
Fabio Muratore <fabio.muratore@famura.net>
2020-06-03 19:29:44 +0200

    Updated sim_potential_dynamics.py

--- Pyrado/scripts/simulation/sim_potential_dynamics.py
+++ Pyrado/scripts/simulation/sim_potential_dynamics.py
@@ -28,7 +28,7 @@ if __name__ == '__main__':
         raise pyrado.TypeErr(given=policy, expected_type=ADNPolicy)
 
     # Define the parameters for evaluation
-    num_steps = 1000
+    num_steps = 50
     p_init_min, p_init_max, num_p_init = -6., 6., 11
     print_cbt(f'Evaluating an ADNPolicy for {num_steps} steps ad {1/env.dt} Hz with initial potentials ranging from'
               f'{p_init_min} to {p_init_max}.', 'c')
@@ -37,20 +37,39 @@ if __name__ == '__main__':
     p_init = to.linspace(p_init_min, p_init_max, num_p_init)  # endpoint included
     num_p = len(policy.potentials)
 
-    fig, ax = plt.subplots(1, figsize=(12, 10), subplot_kw={'projection': '3d'})
-    fig.canvas.set_window_title('Potentials over time for various initial potentials')
+    # Iterate over all action dimensions since we got as many potential-based neurons as action dimensions
+    # For mode = standalone they are currently all the same because all neuron potential-based obey the same dynamics.
+    # However, this does not necessarily have to be that way. Thus we plot the same way as for mode = policy.
+    for a in range(policy.env_spec.act_space.flat_dim):
 
-    for p_0 in p_init:
-        p = to.zeros(num_steps, num_p)
-        s = to.zeros(num_steps, num_p)
-        policy._potentials = p_0*to.ones_like(policy.potentials)
+        fig, ax = plt.subplots(1, figsize=(12, 10), subplot_kw={'projection': '3d'})
+        fig.canvas.set_window_title(f'Potential dynamics for the {a}-th action dimension for initial values')
+        ax.set_xlabel('$t$ [s]')
+        ax.set_ylabel('$p_0$')
+        ax.set_zlabel('$p(t)$')
 
-        for i in range(num_steps):
-            p[i, :] = policy.potentials + env.dt*policy.potentials_dot(stimuli=s[i, :])
-            policy._potentials = p[i, :].clone()
+        for p_0 in p_init:
+            p = to.zeros(num_steps, num_p)
+            s = to.zeros(num_steps, num_p)
+            policy._potentials = p_0*to.ones_like(policy.potentials)
+            hidden = policy.init_hidden()
 
-        # Plot
-        plt.plot(time.numpy(), p_0.repeat(num_steps).numpy(), p[:, 0].detach().numpy())
+            for i in range(num_steps):
+                if args.mode == 'standalone':
+                    # Use the potential dynamics as defined in the loaded ADNPolicy
+                    p[i, :] = policy.potentials + env.dt*policy.potentials_dot(stimuli=s[i, :])
+                    policy._potentials = p[i, :].clone()
+
+                elif args.mode == 'policy':
+                    # Use the loaded ADNPolicy's forward method and pass zero-observations
+                    p[i, :] = policy._potentials.clone()
+                    _, hidden = policy(to.zeros(policy.env_spec.obs_space.shape), hidden)  # previous action is in hidden
+
+                else:
+                    raise pyrado.ValueErr(given=args.mode, eq_constraint="'standalone' or 'policy'")
+
+            # Plot
+            plt.plot(time.numpy(), p_0.repeat(num_steps).numpy(), p[:, a].detach().numpy())
 
     # Save
     if args.save_figures:
