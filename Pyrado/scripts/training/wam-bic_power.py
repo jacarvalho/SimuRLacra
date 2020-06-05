b20e2f7c6773b3a103240b1b4caf362ebbd5a440
Fabio Muratore <fabio.muratore@famura.net>
2020-06-05 10:56:10 +0200

    Custom randomizer and new parameters

--- Pyrado/scripts/training/wam-bic_power.py
+++ Pyrado/scripts/training/wam-bic_power.py
@@ -5,6 +5,8 @@ import numpy as np
 
 from pyrado.algorithms.power import PoWER
 from pyrado.domain_randomization.default_randomizers import get_default_randomizer_wambic
+from pyrado.domain_randomization.domain_parameter import UniformDomainParam
+from pyrado.domain_randomization.domain_randomizer import DomainRandomizer
 from pyrado.environment_wrappers.domain_randomization import DomainRandWrapperLive
 from pyrado.environments.mujoco.wam import WAMBallInCupSim
 from pyrado.logger.experiment import setup_experiment, save_list_of_dicts_to_yaml
@@ -17,26 +19,29 @@ if __name__ == '__main__':
 
     # Environment
     env_hparams = dict(
-        max_steps=1750,
+        max_steps=1500,
         task_args=dict(factor=0.05)
     )
     env = WAMBallInCupSim(**env_hparams)
 
     # Randomizer
-    # env = DomainRandWrapperLive(env, get_default_randomizer_wambic())
+    randomizer = DomainRandomizer(
+        UniformDomainParam(name='cup_scale', mean=1.2, halfspan=0.3)
+    )
+    env = DomainRandWrapperLive(env, randomizer)
 
     # Policy
     policy_hparam = dict(
-        rbf_hparam=dict(num_feat_per_dim=7, bounds=(0., 1.), scale=None),
+        rbf_hparam=dict(num_feat_per_dim=8, bounds=(0., 1.), scale=None),
         dim_mask=2
     )
     policy = DualRBFLinearPolicy(env.spec, **policy_hparam)
 
     # Algorithm
     algo_hparam = dict(
-        max_iter=50,
-        pop_size=100,
-        num_rollouts=1,
+        max_iter=100,
+        pop_size=50,
+        num_rollouts=20,
         num_is_samples=5,
         expl_std_init=np.pi/4,
         expl_std_min=0.02,
