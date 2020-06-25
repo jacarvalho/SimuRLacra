568f435cfc188624a9a0bf57c9c7e0347aa9077d
Fabio Muratore <fabio.muratore@famura.net>
2020-06-25 19:29:22 +0200

    Bug fixes
    (cherry picked from commit 0682448af6793f3983ca1f309214745ff55467fb)

--- Pyrado/pyrado/plotting/rollout_based.py
+++ Pyrado/pyrado/plotting/rollout_based.py
@@ -55,20 +55,21 @@ def plot_observations_actions_rewards(ro: StepSequence):
         fig, axs = plt.subplots(dim_obs + dim_act + 1, 1, figsize=(8, 12))
         fig.suptitle('Observations, Actions, and Reward over Time')
         plt.subplots_adjust(hspace=.5)
+        colors = plt.get_cmap('tab20')(np.linspace(0, 1, dim_obs if dim_obs > dim_act else dim_act))
 
         # Observations (without the last time step)
         for i in range(dim_obs):
-            axs[i].plot(t, ro.observations[:-1, i], label=_get_obs_label(ro, i), c=f'C{i%10}')
+            axs[i].plot(t, ro.observations[:-1, i], label=_get_obs_label(ro, i), c=colors[i])
             axs[i].legend()
 
         # Actions
         for i in range(dim_act):
-            axs[i + dim_obs].plot(t, ro.actions[:, i], label=_get_act_label(ro, i), c=f'C{i%10}')
+            axs[i + dim_obs].plot(t, ro.actions[:, i], label=_get_act_label(ro, i), c=colors[i])
             axs[i + dim_obs].legend()
         # action_labels = env.unwrapped.action_space.labels; label=action_labels[0]
 
         # Rewards
-        axs[-1].plot(t, ro.rewards, label='reward')
+        axs[-1].plot(t, ro.rewards, label='reward', c='k')
         axs[-1].legend()
         plt.show()
 
@@ -99,6 +100,7 @@ def plot_observations(ro: StepSequence, idcs_sel: Sequence[int] = None):
         fig, axs = plt.subplots(nrows=num_rows, ncols=num_cols, figsize=(num_cols*5, num_rows*3))
         fig.suptitle('Observations over Time')
         plt.subplots_adjust(hspace=.5)
+        colors = plt.get_cmap('tab20')(np.linspace(0, 1, len(dim_obs)))
 
         if len(dim_obs) == 1:
             axs.plot(t, ro.observations[:-1, dim_obs[0]], label=_get_obs_label(ro, dim_obs[0]))
@@ -109,7 +111,7 @@ def plot_observations(ro: StepSequence, idcs_sel: Sequence[int] = None):
                     if j + i*num_cols < len(dim_obs):
                         # Omit the last observation for simplicity
                         axs[i, j].plot(t, ro.observations[:-1, j + i*num_cols],
-                                       label=_get_obs_label(ro, j + i*num_cols), c=f'C{i%10}')
+                                       label=_get_obs_label(ro, j + i*num_cols), c=colors[j + i*num_cols])
                         axs[i, j].legend()
                     else:
                         # We might create more subplots than there are observations
@@ -147,6 +149,7 @@ def plot_features(ro: StepSequence, policy: Policy):
         fig, axs = plt.subplots(nrows=num_rows, ncols=num_cols, figsize=(num_cols*5, num_rows*3))
         fig.suptitle('Feature values over Time')
         plt.subplots_adjust(hspace=.5)
+        colors = plt.get_cmap('tab20')(np.linspace(0, 1, dim_feat))
 
         if len(dim_feat) == 1:
             axs.plot(t, feat_vals[:-1, dim_feat[0]], label=_get_obs_label(ro, dim_feat[0]))
@@ -157,7 +160,7 @@ def plot_features(ro: StepSequence, policy: Policy):
                     if j + i*num_cols < len(dim_feat):
                         # Omit the last observation for simplicity
                         axs[i, j].plot(t, feat_vals[:-1, j + i*num_cols],
-                                       label=f'$\phi_{j + i*num_cols}$', c=f'C{i%10}')
+                                       label=rf'$\phi_{j + i*num_cols}$', c=colors[j + i*num_cols])
                         axs[i, j].legend()
                     else:
                         # We might create more subplots than there are observations
@@ -180,6 +183,7 @@ def plot_actions(ro: StepSequence, env: Env = None):
         fig, axs = plt.subplots(dim_act, figsize=(8, 12))
         fig.suptitle('Actions over Time')
         plt.subplots_adjust(hspace=.5)
+        colors = plt.get_cmap('tab20')(np.linspace(0, 1, dim_act))
 
         if env is not None:
             act_space_unnorm = remove_env(env, ActNormWrapper).act_space
@@ -193,7 +197,7 @@ def plot_actions(ro: StepSequence, env: Env = None):
         else:
 
             for i in range(dim_act):
-                axs[i].plot(t, ro.actions[:, i], label=_get_act_label(ro, i), c=f'C{i%10}')
+                axs[i].plot(t, ro.actions[:, i], label=_get_act_label(ro, i), c=colors[i])
                 if env is not None:
                     axs[i].plot(t, act_clipped[:, i], label=_get_act_label(ro, i) + ' (clipped)', c='k', ls='--')
                 axs[i].legend()
@@ -212,42 +216,76 @@ def plot_rewards(ro: StepSequence):
 
         fig, ax = plt.subplots(1)
         fig.suptitle('Reward over Time')
-        ax.plot(t, ro.rewards)
+        ax.plot(t, ro.rewards, c='k')
         plt.show()
 
 
-def plot_adn_data(ro: StepSequence):
+def plot_potentials(ro: StepSequence, layout: str = 'joint'):
     """
-    Plot the trajectories specific to an Activation Dynamic Network (ADN).
+    Plot the trajectories specific to an Activation Dynamic Network (ADN) or a Neural Field (NF) policy.
 
     :param ro: input rollout
+    :param layout: group jointly (default), or create a separate sub-figure for each plot
     """
-    if hasattr(ro, 'actions') and hasattr(ro, 'potentials') and hasattr(ro, 'stimuli'):
+    if (hasattr(ro, 'actions') and hasattr(ro, 'potentials') and
+        hasattr(ro, 'stimuli_external') and hasattr(ro, 'stimuli_internal')):
         # Use recorded time stamps if possible
         t = ro.env_infos.get('t', np.arange(0, ro.length)) if hasattr(ro, 'env_infos') else np.arange(0, ro.length)
-        num_rows = ro.potentials.shape[1]  # number of movement primitives / dynamical systems
+        num_pot = ro.potentials.shape[1]  # number of neurons with potential
+        num_act = ro.actions.shape[1]
+        colors_pot = plt.get_cmap('tab20')(np.linspace(0, 1, num_pot))
+        colors_act = plt.get_cmap('tab20')(np.linspace(0, 1, num_act))
 
-        fig = plt.figure()
-        gs = gridspec.GridSpec(num_rows, 3)
+        if layout == 'separate':
+            fig = plt.figure(figsize=(16, 10))
+            gs = fig.add_gridspec(nrows=num_pot, ncols=4)
+            for i in range(num_pot):
+                ax0 = fig.add_subplot(gs[i, 0])
+                ax0.plot(t, ro.stimuli_external[:, i], label=rf'$s_{{ext,{i}}}$', c=colors_pot[i])
+                ax1 = fig.add_subplot(gs[i, 1])
+                ax1.plot(t, ro.stimuli_internal[:, i], label=rf'$s_{{int,{i}}}$', c=colors_pot[i])
+                ax2 = fig.add_subplot(gs[i, 2], sharex=ax0)
+                ax2.plot(t, ro.potentials[:, i], label=rf'$p_{{{i}}}$', c=colors_pot[i])
+                if i < num_act:  # could have more potentials than actions
+                    ax3 = fig.add_subplot(gs[i, 3], sharex=ax0)
+                    ax3.plot(t, ro.actions[:, i], label=rf'$a_{{{i}}}$', c=colors_act[i])
 
-        for i in range(num_rows):
-            ax0 = fig.add_subplot(gs[i, 0])
-            ax0.plot(t, ro.stimuli[:, i], label=rf'$s_{i}$', c=f'C{i%10}')
-            ax0.legend()
-            ax1 = fig.add_subplot(gs[i, 1], sharex=ax0)
-            ax1.plot(t, ro.potentials[:, i], label=rf'$p_{i}$', c=f'C{i%10}')
-            ax1.legend()
-            ax2 = fig.add_subplot(gs[i, 2], sharex=ax0)
-            ax2.plot(t, ro.actions[:, i], label=rf'$a_{i}$', c=f'C{i%10}')
-            ax2.legend()
+                if i == 0:
+                    ax0.set_title(f'{ro.stimuli_external.shape[1]} External stimuli over time')
+                    ax1.set_title(f'{ro.stimuli_internal.shape[1]} Internal stimuli over time')
+                    ax2.set_title(f'{ro.potentials.shape[1]} Potentials over time')
+                    ax3.set_title(f'{ro.actions.shape[1]} Actions over time')
 
-            if i == 0:
-                ax0.set_title('Stimuli over time')
-                ax1.set_title('Potentials over time')
-                ax2.set_title('Activations over time')
+            # for a in fig.get_axes():
+            #     a.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 
-        plt.subplots_adjust(hspace=.2)
-        plt.subplots_adjust(wspace=.3)
+            plt.subplots_adjust(hspace=.5)
+            plt.subplots_adjust(wspace=.8)
+
+        elif layout == 'joint':
+            fig, axs = plt.subplots(nrows=4, ncols=1, sharex=True, figsize=(8, 12))
+
+            for i in range(num_pot):
+                axs[0].plot(t, ro.stimuli_external[:, i], label=rf'$s_{{ext,{i}}}$', c=colors_pot[i])
+                axs[1].plot(t, ro.stimuli_internal[:, i], label=rf'$s_{{int,{i}}}$', c=colors_pot[i])
+                axs[2].plot(t, ro.potentials[:, i], label=rf'$p_{{{i}}}$', c=colors_pot[i])
+
+            for i in range(num_act):
+                axs[3].plot(t, ro.actions[:, i], label=rf'$a_{{{i}}}$', c=colors_act[i])
+
+            axs[0].set_title(f'{ro.stimuli_external.shape[1]} External stimuli over time')
+            axs[1].set_title(f'{ro.stimuli_internal.shape[1]} Internal stimuli over time')
+            axs[2].set_title(f'{ro.potentials.shape[1]} Potentials over time')
+            axs[3].set_title(f'{ro.actions.shape[1]} Actions over time')
+
+            # for a in fig.get_axes():
+            #     a.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+
+            plt.subplots_adjust(wspace=.8)
+
+        else:
+            raise pyrado.ValueErr(given=layout, eq_constraint='joint or separate')
+
         plt.show()
 
 
