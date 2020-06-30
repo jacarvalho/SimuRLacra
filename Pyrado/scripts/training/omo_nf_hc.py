0a495e1500807bcc787ad6d9c0f156b9c47d8145
Fabio Muratore <fabio.muratore@famura.net>
2020-06-30 08:54:01 +0200

    Found good setting

--- Pyrado/scripts/training/omo_nf_hc.py
+++ Pyrado/scripts/training/omo_nf_hc.py
@@ -4,7 +4,6 @@ Train an agent to solve the Box Shelving task task using Activation Dynamics Net
 import numpy as np
 import torch as to
 
-from pyrado.algorithms.cem import CEM
 from pyrado.algorithms.hc import HCNormal
 from pyrado.environment_wrappers.action_normalization import ActNormWrapper
 from pyrado.environments.pysim.one_mass_oscillator import OneMassOscillatorSim
@@ -23,38 +22,23 @@ if __name__ == '__main__':
 
     # Policy
     policy_hparam = dict(
-        hidden_size=3,
+        hidden_size=5,
         conv_out_channels=1,
         mirrored_conv_weights=True,
         conv_kernel_size=3,
         conv_padding_mode='circular',
         init_param_kwargs=dict(bell=True),
         activation_nonlin=to.sigmoid,
-        tau_init=1e-2,
+        tau_init=1e-1,
         tau_learnable=False,
-        potential_init_learnable=False,
+        potential_init_learnable=True,
     )
     policy = NFPolicy(spec=env.spec, dt=env.dt, **policy_hparam)
     print(policy)
 
-    # Algorithm
-    # algo_hparam = dict(
-    #     max_iter=50,
-    #     pop_size=100,
-    #     num_rollouts=6,
-    #     num_is_samples=10,
-    #     expl_std_init=0.5,
-    #     expl_std_min=0.02,
-    #     extra_expl_std_init=0.5,
-    #     extra_expl_decay_iter=10,
-    #     full_cov=False,
-    #     symm_sampling=False,
-    #     num_sampler_envs=8,
-    # )
-    # algo = CEM(ex_dir, env, policy, **algo_hparam)
     algo_hparam = dict(
         max_iter=100,
-        pop_size=4*policy.num_param,
+        pop_size=2*policy.num_param,
         expl_factor=1.05,
         num_rollouts=6,
         expl_std_init=0.3,
